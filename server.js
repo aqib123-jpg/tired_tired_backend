e5a867b704c9b7ad77046bd95c2d54a90d5fd1f4
aqib123-jpg <aqibk5867@gmail.com>
2024-12-22 16:32:26 +0500

    maaf kro

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const PORT =  3001;
@@ -10,21 +10,6 @@ const PORT =  3001;
 app.use(cors());
 app.use(express.json());
 
-// Database Connection
-const db = mysql.createConnection({
-  host: 'localhost', 
-  user: 'root',
-  password: 'MySqL321@',
-  database:'CHECKINGECOMMERCE',
-});
-
-db.connect(err => {
-  if (err) {
-      console.error('Error connecting to MySQL:', err.message);
-      process.exit(1); // Exit if connection fails
-  }
-  console.log('Connected to MySQL database');
-});
 
 // Routes
 app.get('/', (req, res) => {
